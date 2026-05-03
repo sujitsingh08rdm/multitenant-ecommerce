b79913019944587fd4c8d34a1810c37022041599
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2026-05-03 18:36:48 +0530

    04 Payload integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,6 @@
+import type { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  fields: [{ name: "name", type: "text", required: true }],
+};
