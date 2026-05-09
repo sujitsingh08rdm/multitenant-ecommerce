af2acc0d897011113580d3e21f90e5980723c777
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2026-05-09 20:09:30 +0530

    06: categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Category } from "@/payload-types";
 
 import { useRef, useState } from "react";
 import { useDropdownPosition } from "./use-dropdown-position";
 import SubcategoryMenu from "./subcategory-menu";
+import { customCategory } from "./types";
+import Link from "next/link";
 
 interface Props {
-  category: Category;
+  category: customCategory;
   isActive?: boolean;
   isNavigationHovered?: boolean;
 }
@@ -32,6 +33,12 @@ export default function CategoryDropdown({
     setIsOpen(false);
   };
 
+  // const toggleDropdown = () => {
+  //   if (category?.subcategories?.docs?.length) {
+  //     setIsOpen(!isOpen);
+  //   }
+  // };
+
   const dropdownPosition = getDropdownPosition();
 
   return (
@@ -40,6 +47,7 @@ export default function CategoryDropdown({
       ref={dropdownRef}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      // onClick={toggleDropdown}
     >
       <div className="relative">
         <Button
@@ -47,9 +55,12 @@ export default function CategoryDropdown({
           className={cn(
             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary",
             isActive && !isNavigationHovered && "bg-white border-primary ",
+            isOpen ? "bg-white border-primary" : "",
           )}
         >
-          {category.name}
+          <Link href={`/${category.slug === "all" ? "" : category.slug}`}>
+            {category.name}
+          </Link>
         </Button>
         {category.subcategories && category.subcategories.length > 0 && (
           <div
